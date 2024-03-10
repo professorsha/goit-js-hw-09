39a284459aed8443a5852a75176661e028322232
professorsha <69015177+professorsha@users.noreply.github.com>
2024-03-10 10:27:03 +0000

    Deploying to gh-pages from @ professorsha/goit-js-hw-09@04a53345538400c31ce08927da038d41aad7636e 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/styles-bbc74318.js";const l=document.querySelector("form.feedback-form");let o=document.querySelector('[name="email"]'),m=document.querySelector('[name="message"]');l.addEventListener("input",n);l.addEventListener("submit",r);let a=localStorage.getItem("feedback-form-state");if(a){const e=JSON.parse(a);o.value=e.email,m.value=e.message}function n(e){e.preventDefault();const t={email:o.value,message:m.value},s=JSON.stringify(t);localStorage.setItem("feedback-form-state",s)}function r(e){e.preventDefault();const t=JSON.parse(a);console.log(`Об'єкт feedback-form-state:
- email: ${t.email} 
- message: ${t.message}`),document.getElementById("email").value="",m.value="",localStorage.removeItem("feedback-form-state")}
+import"./assets/styles-bbc74318.js";const o=document.querySelector("form.feedback-form");let l=document.querySelector('[name="email"]'),m=document.querySelector('[name="message"]');o.addEventListener("input",s);o.addEventListener("submit",n);let t=localStorage.getItem("feedback-form-state");if(t){let e=JSON.parse(t);l.value=e.email,m.value=e.message}function s(e){e.preventDefault();const a={email:l.value,message:m.value};let r=JSON.stringify(a);localStorage.setItem("feedback-form-state",r)}function n(e){e.preventDefault();let a=JSON.parse(t);console.log(`Об'єкт feedback-form-state:
+ email: ${a.email} 
+ message: ${a.message}`),l.value="",m.value="",localStorage.removeItem("feedback-form-state")}
 //# sourceMappingURL=commonHelpers2.js.map
