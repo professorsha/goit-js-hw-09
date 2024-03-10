86534d2f5fd14508527827abdff5d7f986eafeb4
professorsha <69015177+professorsha@users.noreply.github.com>
2024-03-10 12:04:49 +0200

    update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,25 +1,36 @@
-const formSubmit = document.querySelector('form.feedback-form');
-formSubmit.addEventListener("input", hadlerSubmit);
+// Під час сабміту форми очищай сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та
+//  їхніми поточними значеннями.
 
+const formSubmit = document.querySelector('form.feedback-form');
+let email = document.querySelector('[name="email"]');
+let message = document.querySelector('[name="message"]');
+formSubmit.addEventListener('input', hadlerInput);
+formSubmit.addEventListener('submit', hadlerSubmit);
+let value = localStorage.getItem('feedback-form-state');
+if (value) {
+  const data = JSON.parse(value);
+  email.value = data.email;
+  message.value = data.message;
+}
+
+function hadlerInput(event) {
+  event.preventDefault();
+
+  const feedbackForm = {
+    email: email.value,
+    message: message.value,
+  };
+  const jsn = JSON.stringify(feedbackForm);
+  localStorage.setItem('feedback-form-state', jsn);
+}
 function hadlerSubmit(event) {
   event.preventDefault();
-  alert('gjkfdjghldfj')
-//   const currentImage = event.target;
-//   if (currentImage === event.currentTarget) return;
-
-//   const instance = basicLightbox.create(
-//     `
-//     <img src="${currentImage.dataset.source}" width="800" height="600">
-//   `,
-//     {
-//       onShow: () => document.addEventListener("keydown", onKeyPress),
-//       onClose: () => document.removeEventListener("keydown", onKeyPress),
-//     }
-//   );
-//   const onKeyPress = (e) => {
-//     if (e.key === "Escape") {
-//       instance.close();
-//     }
-//   };
-//   instance.show();
+  const data = JSON.parse(value);
+  console.log(
+    `Об'єкт feedback-form-state:\n email: ${data.email} \n message: ${data.message}`
+  );
+  document.getElementById('email').value = '';
+  
+  message.value = '';
+  localStorage.removeItem('feedback-form-state');
 }
