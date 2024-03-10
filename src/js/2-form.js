04a53345538400c31ce08927da038d41aad7636e
professorsha <69015177+professorsha@users.noreply.github.com>
2024-03-10 12:26:11 +0200

    update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,6 +1,3 @@
-// Під час сабміту форми очищай сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та
-//  їхніми поточними значеннями.
-
 const formSubmit = document.querySelector('form.feedback-form');
 let email = document.querySelector('[name="email"]');
 let message = document.querySelector('[name="message"]');
@@ -8,7 +5,7 @@ formSubmit.addEventListener('input', hadlerInput);
 formSubmit.addEventListener('submit', hadlerSubmit);
 let value = localStorage.getItem('feedback-form-state');
 if (value) {
-  const data = JSON.parse(value);
+  let data = JSON.parse(value);
   email.value = data.email;
   message.value = data.message;
 }
@@ -20,17 +17,17 @@ function hadlerInput(event) {
     email: email.value,
     message: message.value,
   };
-  const jsn = JSON.stringify(feedbackForm);
+  let jsn = JSON.stringify(feedbackForm);
   localStorage.setItem('feedback-form-state', jsn);
 }
 function hadlerSubmit(event) {
   event.preventDefault();
-  const data = JSON.parse(value);
+  let data = JSON.parse(value);
   console.log(
     `Об'єкт feedback-form-state:\n email: ${data.email} \n message: ${data.message}`
   );
-  document.getElementById('email').value = '';
-  
+  email.value = '';
+
   message.value = '';
   localStorage.removeItem('feedback-form-state');
 }
